8667145dd13f32dd973564bb4258aa1cc38de0b3
Iqbalrez <iqbalrr02@gmail.com>
2023-11-16 10:09:17 +0700

    Add SweetAlert2 and Datatable libraries.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,15 @@
     <!-- Fonts -->
     <link href="https://fonts.cdnfonts.com/css/plus-jakarta-sans" rel="stylesheet">
 
+    <!-- Sweet Alert 2 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.27/dist/sweetalert2.min.css">
+
+    <!-- Datatable -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/themes/base/jquery-ui.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/v/ju/dt-1.13.6/r-2.5.0/datatables.min.css" rel="stylesheet">
+
+    @stack('css-internal')
+
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
@@ -23,6 +32,19 @@
             {{ $slot }}
         </div>
     </div>
+
+    <!-- SweetAlert2 -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.27/dist/sweetalert2.all.min.js"></script>
+
+    <!-- Jquery -->
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"
+        integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+
+    <!-- Datatable -->
+    <script src="https://cdn.datatables.net/v/ju/dt-1.13.6/r-2.5.0/datatables.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/jquery-ui.min.js"></script>
+
+    @stack('js-internal')
 </body>
 
 </html>
